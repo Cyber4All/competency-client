88e2a10a13756debf9c95d687d853fe556bef661
Paige Zaleppa <pz0342@gmail.com>
2021-08-18 11:30:25 -0400

    Add core and routes

---
+++ src/app/core/competency.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompetencyService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllCompetencies() {
+
+  }
+
+  createCompetency() {
+
+  }
+
+  editCompetency() {
+
+  }
+
+  lockCompetency() {
+
+  }
+}
---
+++ src/environments/routes.ts
@@ -0,0 +1,24 @@
+import { environment } from './environment';
+export const USER_ROUTES = {
+    LOGIN() {
+        return `${environment.apiURL}/users/login`;
+    },
+    GET_ALL_USERS() {
+        return `${environment.apiURL}/users`;
+    }
+}
+
+export const COMPETENCY_ROUTES = {
+    GET_ALL_COMPETENCIES(){
+        return `${environment.apiURL}/competencies`;
+    },
+    CREATE_COMPETENCY() {
+        return `${environment.apiURL}/competencies`;
+    },
+    EDIT_COMPETENCY(competencyId: string) {
+        return `${environment.apiURL}/competencies/${encodeURIComponent(competencyId)}`;
+    },
+    LOCK_COMPETENCY(competencyId: string) {
+        return `${environment.apiURL}/competencies/${encodeURIComponent(competencyId)}/lock`;
+    }
+}
