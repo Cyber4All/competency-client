fbaa71519501af90323bf8ca3b09e5dc368a6aa8
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2021-08-18 11:28:54 -0400

    created page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { CompetenciesDashboardComponent } from './pages/competencies-dashboard/competencies-dashboard.component';
+import { CompetencyBuilderComponent } from './pages/competencies-dashboard/competency-builder/competency-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CompetenciesDashboardComponent,
+    CompetencyBuilderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/competencies-dashboard/competencies-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competencies-dashboard',
+  templateUrl: './competencies-dashboard.component.html',
+  styleUrls: ['./competencies-dashboard.component.scss']
+})
+export class CompetenciesDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
