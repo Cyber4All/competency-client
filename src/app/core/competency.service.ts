098060d50cbb614d772e833c7b6eb2533860266b
Paige Zaleppa <pz0342@gmail.com>
2021-08-18 15:03:50 -0400

    Commit the service!

--- src/app/core/competency.service.ts
+++ src/app/core/competency.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import {
   HttpClient,
-  HttpHeaders,
-  HttpErrorResponse
 } from '@angular/common/http';
+import { COMPETENCY_ROUTES } from 'src/environments/routes';
 
 @Injectable({
   providedIn: 'root'
@@ -13,18 +12,41 @@ export class CompetencyService {
   constructor(private http: HttpClient) { }
 
   getAllCompetencies() {
-
+    return this.http
+      .get(
+        COMPETENCY_ROUTES.GET_ALL_COMPETENCIES(),
+      )
+      .toPromise();
   }
 
-  createCompetency() {
-
+  createCompetency(competency: any) {
+    return this.http
+      .post(
+        COMPETENCY_ROUTES.CREATE_COMPETENCY(),
+        { competency },
+        { withCredentials: true, responseType: 'text' }
+      )
+      .toPromise();
   }
 
-  editCompetency() {
-
+  editCompetency(competency: any) {
+    return this.http
+      .patch(
+        COMPETENCY_ROUTES.EDIT_COMPETENCY(competency._id),
+        { competency },
+        { withCredentials: true, responseType: 'text' }
+      )
+      .toPromise();
   }
 
-  lockCompetency() {
-
+  lockCompetency(competency: any, lock: boolean) {
+    return this.http
+      .patch(
+        COMPETENCY_ROUTES.EDIT_COMPETENCY(competency._id),
+        { lock },
+        { withCredentials: true, responseType: 'text' }
+      )
+      .toPromise();
   }
 }
+
