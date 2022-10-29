a74ba65f52b3b401f5c395a8c05723d771575b86
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2022-10-29 15:53:55 -0400

    hooked up with degree card; might regret it

--- src/app/shared/components/competency-card/components/degree-card/degree-card.component.ts
+++ src/app/shared/components/competency-card/components/degree-card/degree-card.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, DoCheck, Output, EventEmitter, OnChanges, ChangeDetectorRef, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Degree } from 'src/entity/degree';
 @Component({
-  selector: 'app-degree-card',
+  selector: 'cc-degree-card',
   templateUrl: './degree-card.component.html',
   styleUrls: ['./degree-card.component.scss']
 })
-export class DegreeCardComponent implements OnInit {
+export class DegreeCardComponent implements OnInit, DoCheck {
+
+  @Input() isEdit = false;
+  @Input() currIndex: number | null = null;
+  @Input() degree!: Degree;
+  @Output() degreeChange = new EventEmitter<{update: string, value: Degree}>();
+  @Output() setIndex = new EventEmitter<number>();
+  @Output() nextView = new EventEmitter();
+  @Output() prevView = new EventEmitter();
+  complete = new FormControl('', [Validators.required]);
+  correct = new FormControl('', [Validators.required]);
+  time = new FormControl('', [Validators.required]);
 
   constructor() { }
 
   ngOnInit(): void {
+    // If value exists, set type form control
+    if(this.degree.complete) {
+      this.complete.patchValue(this.degree.complete);
+    }
+    // If value exists, set details form control
+    if (this.degree.time) {
+      this.time.patchValue(this.degree.time);
+    }
+    // If value exists, set workrole form control
+    if (this.degree.correct) {
+      this.correct.patchValue(this.degree.correct);
+    }
+  }
+
+  ngDoCheck(): void {
+    // If any value updates, update parent component
+    if(this.degree.complete || this.degree.correct || this.degree.time) {
+      this.degreeChange.emit({
+        update: 'condition',
+        value: {
+          _id: this.degree._id,
+          complete: this.complete.value,
+          correct: this.correct.value,
+          time: this.time.value
+        }
+      });
+    };
+  }
+
+  /**
+   * Method to set view of builder element
+   *
+   * @param val value of current builder element
+   */
+   setStep(val: number) {
+    this.setIndex.emit(val);
+  }
+
+  /**
+   * Method to advance to next step
+   */
+  nextStep() {
+    if(this.complete.valid && this.correct.valid && this.time.valid) {
+      this.nextView.emit();
+    }
+  }
+
+  prevStep() {
+    this.prevView.emit();
   }
 
 }
