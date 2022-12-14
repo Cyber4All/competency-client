52922d53cab29cb8eb4c40583b12239532fd0205
Paulo Manalo <pmanal1@students.towson.edu>
2022-12-14 14:36:18 -0500

    Creates file upload logic for service

--- src/app/core/file.service.ts
+++ src/app/core/file.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { COMPETENCY_ROUTES } from 'src/environments/routes';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  async uploadFile(competencyId: string, description: string) {
+    const res = await this.http.post(
+      COMPETENCY_ROUTES.CREATE_DOCUMENTATION(competencyId),
+      {
+        userId: this.authService.user?._id,
+        description: description,
+        uri: '', // to be implemented once Lambda route works in prod
+      }
+      );
+    return res;
+  }
 }
