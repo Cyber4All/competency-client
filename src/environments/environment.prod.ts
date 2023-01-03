d7229e1f908a9b9a4024fb3aaf0dd89daea0980d
Paige Zaleppa <pz0342@gmail.com>
2023-01-03 08:30:27 -0500

    Add border to cards

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   experimental: false,
   host: '',
-  apiURL: 'http://cc-api-staging-ext-alb-1271008437.us-east-1.elb.amazonaws.com:3000',
+  apiURL: 'https://cc-ecosystem-api.yeetbot.click',
   cardOrganizationUrl: 'https://api-gateway.caeresource.directory/organizations?type=&verified=verified&mine=&sort='
 };
