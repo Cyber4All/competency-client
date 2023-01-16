bd37fa5d31b55e880c4e389b4340812b01b8df0d
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2023-01-16 10:50:47 -0500

    formatted route guard

--- src/app/shared/guards/admin.guard.ts
+++ src/app/shared/guards/admin.guard.ts
@@ -1,26 +1,35 @@
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
     providedIn: 'root'
-  })
+})
 export class AdminGuard implements CanActivate {
-    constructor(private auth: AuthService, private router: Router,){
-    }
-     canActivate(
+
+    constructor(
+        private auth: AuthService,
+        private router: Router
+    ) { }
+
+    async canActivate(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
-    ): boolean {
-        let val = false;
-        this.auth.isAdmin.subscribe((isAdmin: boolean)=>{
-            if(isAdmin){
-                val = true;
-            }else{
-                this.router.navigate(['/dashboard']);
-                val = false;
+    ): Promise<boolean> {
+        let adminUser = false;
+        // Validate user actions for admin access
+        await this.auth.validateAdminAccess();
+        this.auth.isAdmin.subscribe((isAdmin: boolean) => {
+            if(isAdmin) {
+                adminUser = true;
+            } else {
+                adminUser = false;
             }
         });
-        return val;
+        // If not admin; redirect to author dashboard
+        if (!adminUser) {
+            this.router.navigate(['/dashboard']);
+        }
+        return adminUser;
     }
-    }
+}
