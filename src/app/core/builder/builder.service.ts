6d7354a24618946ee2286fef403d61c7bd37d20e
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2023-01-26 21:24:08 -0500

    stupid untracked files

---
+++ src/app/core/builder/builder.service.ts
@@ -0,0 +1,159 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { lastValueFrom } from 'rxjs';
+import { COMPETENCY_ROUTES } from '../../../environments/routes';
+import { Actor } from '../../../entity/actor';
+import { Degree } from '../../../entity/degree';
+import { Condition } from '../../../entity/condition';
+import { Behavior } from '../../../entity/behavior';
+import { Employability } from '../../../entity/employability';
+import { Notes } from '../../../entity/notes';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BuilderService {
+    constructor(
+        private http: HttpClient,
+        private auth: AuthService,
+    ) {}
+
+    /**
+     * Method to create the shell of a competency
+     *
+     * @returns new competency id
+     */
+    async createCompetency() {
+        this.auth.initHeaders();
+        return await lastValueFrom(this.http
+        .post(
+            COMPETENCY_ROUTES.CREATE_COMPETENCY(),
+            {},
+            { headers: this.auth.headers, withCredentials: true, responseType: 'json' }
+        ))
+        .catch((e)=> {
+            console.log(e);
+        });
+    }
+
+    /**
+     * Method to send actor updates
+     *
+     * @param competencyId current competency being edited
+     * @param actorUpdate updated fields of an actor attribute
+     * @returns null for successful request
+     */
+    async updateActor(competencyId: string, actorUpdate: Actor) {
+        this.auth.initHeaders();
+        return await lastValueFrom(this.http
+        .patch(
+            COMPETENCY_ROUTES.UPDATE_ACTOR(competencyId),
+            actorUpdate,
+            { headers: this.auth.headers, withCredentials: true, responseType: 'json' }
+        ))
+        .catch((e)=> {
+            console.log(e);
+        });
+    }
+
+    /**
+     * Method to send behavior updates
+     *
+     * @param competencyId current competency being edited
+     * @param behaviorUpdate udpated fields of a behavior attribute
+     * @returns mull for succesful request
+     */
+    async updateBehavior(competencyId: string, behaviorUpdate: Partial<Behavior>) {
+        this.auth.initHeaders();
+        return await lastValueFrom(this.http
+        .patch(
+            COMPETENCY_ROUTES.UPDATE_BEHAVIOR(competencyId),
+            behaviorUpdate,
+            { headers: this.auth.headers, withCredentials: true, responseType: 'json' }
+        ))
+        .catch((e)=> {
+            console.log(e);
+        });
+    }
+
+    /**
+     * Method to send condition updates
+     *
+     * @param competencyId current competncy being edited
+     * @param conditionUpdate updated fields of a condition attribute
+     * @returns null for successful request
+     */
+    async updateCondition(competencyId: string, conditionUpdate: Partial<Condition>) {
+        this.auth.initHeaders();
+        return await lastValueFrom(this.http
+        .patch(
+            COMPETENCY_ROUTES.UPDATE_CONDITION(competencyId),
+            conditionUpdate,
+            { headers: this.auth.headers, withCredentials: true, responseType: 'json' }
+        ))
+        .catch((e)=> {
+            console.log(e);
+        });
+    }
+
+    /**
+     * Method to send degree updates
+     *
+     * @param competencyId current competency being edited
+     * @param degreeUpdate updated fields of a degree attribute
+     * @returns null for successful request
+     */
+    async updateDegree(competencyId: string, degreeUpdate: Partial<Degree>) {
+        this.auth.initHeaders();
+        return await lastValueFrom(this.http
+        .patch(
+            COMPETENCY_ROUTES.UPDATE_DEGREE(competencyId),
+            degreeUpdate,
+            { headers: this.auth.headers, withCredentials: true, responseType: 'json' }
+        ))
+        .catch((e)=> {
+            console.log(e);
+        });
+    }
+
+    /**
+     * Method to send employability updates
+     *
+     * @param competencyId current competency being edited
+     * @param employabilityUpdate updated fields of a employability attribute
+     * @returns null for successful request
+     */
+    async updateEmployability(competencyId: string, employabilityUpdate: Partial<Employability>) {
+        this.auth.initHeaders();
+        return await lastValueFrom(this.http
+        .patch(
+            COMPETENCY_ROUTES.UPDATE_EMPLOYABILITY(competencyId),
+            employabilityUpdate,
+            { headers: this.auth.headers, withCredentials: true, responseType: 'json' }
+        ))
+        .catch((e)=> {
+            console.log(e);
+        });
+    }
+
+    /**
+     * Method to send notes updates
+     *
+     * @param competencyId current competency being edited
+     * @param notesUpdate updated fields of notes attribute
+     * @returns null for successful request
+     */
+    async updateNotes(competencyId: string, notesUpdate: Partial<Notes>) {
+        this.auth.initHeaders();
+        return await lastValueFrom(this.http
+        .patch(
+            COMPETENCY_ROUTES.UPDATE_NOTES(competencyId),
+            notesUpdate,
+            { headers: this.auth.headers, withCredentials: true, responseType: 'json' }
+        ))
+        .catch((e)=> {
+            console.log(e);
+        });
+    }
+}
---
+++ src/app/core/builder/competency-builder.class.ts
@@ -0,0 +1,129 @@
+import { Actor } from '../../../entity/actor';
+import { Behavior } from '../../../entity/behavior';
+import { Competency } from '../../../entity/competency';
+import { Condition } from '../../../entity/condition';
+import { Degree } from '../../../entity/degree';
+import { Documentation } from '../../../entity/documentation';
+import { Employability } from '../../../entity/employability';
+import { Lifecycles } from '../../../entity/lifecycles';
+import { Notes } from '../../../entity/notes';
+
+export class CompetencyBuilder extends Competency {
+    _id!: string;
+    status!: Lifecycles;
+    authorId!: string;
+    version!: number;
+    actor!: Actor;
+    behavior!: Behavior;
+    condition!: Condition;
+    degree!: Degree;
+    employability!: Employability;
+    notes!: Notes;
+
+    constructor(
+        _id: string,
+        status: Lifecycles,
+        authorId: string,
+        version: number,
+        actor: Actor,
+        behavior: Behavior,
+        condition: Condition,
+        degree: Degree,
+        employability: Employability,
+        notes: Notes
+    ) {
+        super(
+            _id,
+            status,
+            authorId,
+            version,
+            actor,
+            behavior,
+            condition,
+            degree,
+            employability,
+            notes
+        );
+
+        this._id = _id;
+        this.status = status;
+        this.authorId = authorId;
+        this.version = version;
+        this.actor = actor;
+        this.behavior = behavior;
+        this.condition = condition;
+        this.degree = degree;
+        this.employability = employability;
+        this.notes = notes;
+    }
+
+    setActor(update: {details: string, type: string}) {
+        this.actor = update;
+        return this;
+    }
+
+    setBehavior(update: {tasks: string, work_role: string, details: string}) {
+        this.behavior = update;
+        return this;
+    }
+
+    setCondition(update: {limitations: string, tech: string[], documentation: Documentation[]}) {
+        this.condition = update;
+        return this;
+    }
+
+    setDegree(update: {time: string, correct: string, complete: string}) {
+        this.degree = update;
+        return this;
+    }
+
+    setEmployability(update: {details: string}) {
+        this.employability = update;
+        return this;
+    }
+
+    setNotes(updates: {details: string}) {
+        this.notes = updates;
+        return this;
+    }
+
+    build() {
+        if (!this._id || this._id === null || this._id === undefined) {
+            throw new Error('Competency ID is not valid');
+        }
+        if (!this.authorId || this.authorId === null || this.authorId === undefined) {
+            throw new Error('Competency author is not valid');
+        }
+        if (!(this.status in Lifecycles)) {
+            throw new Error('Competency is not in a valid state');
+        }
+        if (!this.actor.details || !this.actor.type) {
+            throw new Error('Actor is incomplete');
+        }
+        if (!this.behavior.details || !this.behavior.tasks || !this.behavior.work_role) {
+            throw new Error('Behavior is incomplete');
+        }
+        if (!this.condition.limitations || !this.condition.tech) {
+            throw new Error('Condition is incomplete');
+        }
+        if (!this.degree.time || !this.degree.complete || !this.degree.correct) {
+            throw new Error('Degree is incomplete');
+        }
+        if (!this.employability.details) {
+            throw new Error('Employability is incomplete');
+        }
+        return new Competency(
+            this._id,
+            this.status,
+            this.authorId,
+            this.version,
+            this.actor,
+            this.behavior,
+            this.condition,
+            this.degree,
+            this.employability,
+            this.notes
+        );
+    }
+
+}
