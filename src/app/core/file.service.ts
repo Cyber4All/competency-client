d1419ca6b37568fcb2be32b20af54ef4320c6a13
Paulo Manalo <pmanal1@students.towson.edu>
2023-02-02 15:38:45 -0500

    Adds catches to S3 function calls

--- src/app/core/file.service.ts
+++ src/app/core/file.service.ts
@@ -48,7 +48,9 @@ export class FileService {
     formData.append('file', file);
 
     // Adds the file to S3
-    await lastValueFrom(this.http.post(lambdaResponse.url, formData));
+    await lastValueFrom(this.http.post(lambdaResponse.url, formData)).catch(e => {
+      console.log(e); // TODO: Swap for toaster component
+    });
 
     // formats the uri to be stored in mongo, will be used for file retrieval and deleting a file
     const fileURL = `https://cc-file-upload-bucket.s3.amazonaws.com/${this.authService.user?._id}/${competencyId}/${file.name}`;
@@ -81,7 +83,15 @@ export class FileService {
     const docsToDelete = (documentation instanceof Array) ? documentation : [documentation];
     const fileNameQuery = docsToDelete.map(doc => this.parseFileName(doc.uri)).join(',');
     const LambdaResponse: { urls: string[] } = await this.deleteLambdaService(competencyId, fileNameQuery);
-    await Promise.all(LambdaResponse.urls.map(url => lastValueFrom(this.http.delete(url))));
+    await Promise.all(LambdaResponse.urls.map(url => {
+      try {
+        lastValueFrom(this.http.delete(url)).then(res => {
+          console.log(res); // TODO: Swap for toaster component, this is a success toaster
+        });
+      } catch (error) {
+        console.log(error); // TODO: Swap for toaster component
+      }
+    }));
     await lastValueFrom(
       this.http.delete(
         COMPETENCY_ROUTES.DELETE_DOCUMENTATION(competencyId),
