448700d6241d6ac2685fa675f2358a2a8b6a9d87
Bereket Sega <bsega1@secured-admins-MacBook-Pro.local>
2023-02-15 13:28:24 -0500

    no downtime right now

--- src/app/core/banner.service.ts
+++ src/app/core/banner.service.ts
@@ -61,6 +61,6 @@ export class BannerService {
      */
     async getDowntime(): Promise<Downtime> {
     //return await lastValueFrom(this.http.get<Downtime>(UTITLITY_ROUTES.GET_DOWNTIME()))
-      return {'isDown': true, 'message': 'The service is down!'};
+      return {'isDown': false, 'message': undefined};
      }
 }
