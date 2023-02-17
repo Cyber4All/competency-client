2951a8832748181488838a0d9b4e32f2aeed16df
Bereket Sega <bsega1@secured-admins-MacBook-Pro.local>
2023-02-17 10:23:28 -0500

    added downtime page

---
+++ src/app/shared/components/downtime/downtime.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cc-downtime',
+  templateUrl: './downtime.component.html',
+  styleUrls: ['./downtime.component.scss']
+})
+export class DowntimeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
