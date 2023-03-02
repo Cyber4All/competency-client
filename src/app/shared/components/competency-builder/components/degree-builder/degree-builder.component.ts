3c2fb58ea2c9266a5a1a1ccc2f225956dd37453d
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2023-03-02 18:42:01 -0500

    implemented degree validation

--- src/app/shared/components/competency-builder/components/degree-builder/degree-builder.component.ts
+++ src/app/shared/components/competency-builder/components/degree-builder/degree-builder.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnChanges, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { debounceTime } from 'rxjs';
+import { BuilderValidation } from '../../../../../../entity/builder-validation';
 import { Degree } from '../../../../../../entity/degree';
 import { BuilderService } from '../../../../../core/builder/builder.service';
 @Component({
@@ -12,6 +13,7 @@ export class DegreeBuilderComponent implements OnInit {
 
   @Input() degree!: Degree;
   @Output() degreeChange = new EventEmitter<{update: string, value: Degree}>();
+  degreeErrors: BuilderValidation[] = [];
   templateIndex = 0;
   complete = new FormControl('');
   correct = new FormControl('');
@@ -22,12 +24,33 @@ export class DegreeBuilderComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // Subscribe to degree errors
+    this.builderService.degreeErrors.subscribe((errors: BuilderValidation[]) => {
+      errors.map((error: BuilderValidation) => {
+        // If error is not already in local degreeErrors array, add it
+        if (this.degreeErrors.findIndex((degreeError: BuilderValidation) => {
+          return degreeError.attribute === error.attribute;
+        }) === -1) {
+          this.degreeErrors.push(error);
+        }
+        // Set form validation errors
+        this.displayErrors();
+      });
+    });
+    // Subscribe to degree template index
     this.builderService.templateIndex.subscribe((index: number) => {
       this.templateIndex = index;
     });
+    // Subscribe to complete form control
     this.complete.valueChanges
       .pipe(debounceTime(1000))
       .subscribe((completeChange: string) => {
+        // Remove complete error from degreeErrors array
+        this.degreeErrors = this.degreeErrors.filter((error: BuilderValidation) => {
+          return error.attribute !== 'complete';
+        });
+        this.complete.setErrors({error: false});
+        // Emit degree complete change to parent builder component
         this.degreeChange.emit({
           update: 'degree',
           value: {
@@ -38,9 +61,16 @@ export class DegreeBuilderComponent implements OnInit {
           }
         });
       });
+    // Subscribe to correct form control
     this.correct.valueChanges
       .pipe(debounceTime(1000))
       .subscribe((correctChange: string) => {
+        // Remove correct error from degreeErrors array
+        this.degreeErrors = this.degreeErrors.filter((error: BuilderValidation) => {
+          return error.attribute !== 'correct';
+        });
+        this.correct.setErrors({error: false});
+        // Emit degree correct change to parent builder component
         this.degreeChange.emit({
           update: 'degree',
           value: {
@@ -51,9 +81,16 @@ export class DegreeBuilderComponent implements OnInit {
           }
         });
       });
+    // Subscribe to time form control
     this.time.valueChanges
       .pipe(debounceTime(1000))
       .subscribe((timeChange) => {
+        // Remove time error from degreeErrors array
+        this.degreeErrors = this.degreeErrors.filter((error: BuilderValidation) => {
+          return error.attribute !== 'time';
+        });
+        this.time.setErrors({error: false});
+        // Emit degree time change to parent builder component
         this.degreeChange.emit({
           update: 'degree',
           value: {
@@ -76,5 +113,23 @@ export class DegreeBuilderComponent implements OnInit {
     if (this.degree.correct) {
       this.correct.patchValue(this.degree.correct);
     }
+  }
+
+  displayErrors(): void {
+    // Iterate through degreeErrors
+    this.degreeErrors.map((error: BuilderValidation) => {
+      // If error is for complete, set complete form control error
+      if (error.attribute === 'complete') {
+        this.complete.setErrors({error: true});
+      }
+      // If error is for correct, set correct form control error
+      if (error.attribute === 'correct') {
+        this.correct.setErrors({error: true});
+      }
+      // If error is for time, set time form control error
+      if (error.attribute === 'time') {
+        this.time.setErrors({error: true});
+      }
+    });
   }
 }
