0b18b263a3cb95103362a1d118ce004fd85e0875
Katelyn Blomquist <katelyn.blomquist@gmail.com>
2023-03-23 10:26:22 -0400

    styling take 1: Getting it working

--- src/app/shared/components/competency-builder/components/builder-navbar/builder-navbar.component.ts
+++ src/app/shared/components/competency-builder/components/builder-navbar/builder-navbar.component.ts
@@ -1,13 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BuilderService } from '../../../../../core/builder/builder.service';
+import { trigger, transition, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'cc-builder-navbar',
   templateUrl: './builder-navbar.component.html',
-  styleUrls: ['./builder-navbar.component.scss']
+  styleUrls: ['./builder-navbar.component.scss'],
+  animations: [
+    trigger('isHighlighed', [
+      state('highlighted', style({
+        backgroundColor: '#E9F0FE',
+        color: '#376ED6',
+        fontWeight: 600,
+      })),
+      state('notHighlighted', style({
+        color: '#AAAAAA'
+      })),
+      transition('isHighlighted => notHighlighted', [
+        animate('4s')
+      ]),
+      transition('notHightlighted => isHighlighted', [
+        animate('4s')
+      ])
+    ])
+  ]
 })
 export class BuilderNavbarComponent implements OnInit {
   currIndex!: number;
+  isHighlighted = {
+    actor: true, // LearningObjects starts highlighted
+    behavior: false,
+    context: false,
+    degree: false,
+    employability: false,
+    notes: false,
+    review: false,
+  };
 
   constructor(
     public builderService: BuilderService,
@@ -18,5 +46,7 @@ export class BuilderNavbarComponent implements OnInit {
       this.currIndex = index;
     });
   }
-
+  isOtherChoiceSelected() {
+    return Object.values(this.isHighlighted).every((isSelected) => !isSelected);
+  }
 }
