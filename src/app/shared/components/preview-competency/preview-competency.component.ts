ad0e063ee90f3a6015b3dace03a51f709a5aa2b4
Paulo Manalo <pmanal1@students.towson.edu>
2023-03-31 16:19:53 -0400

    Completes footer section, adapts header and footer for different lifecycles

--- src/app/shared/components/preview-competency/preview-competency.component.ts
+++ src/app/shared/components/preview-competency/preview-competency.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CompetencyBuilder } from 'src/app/core/builder/competency-builder.class';
+import { Lifecycles } from 'src/entity/lifecycles';
 import { User } from 'src/entity/user';
 
 @Component({
@@ -19,11 +20,57 @@ export class PreviewCompetencyComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.competency.status = Lifecycles.SUBMITTED;
     this.competency.behavior = {
       tasks: ['T0137: Maintain database management systems software.'],
       work_role: 'Database Administrator',
       details: 'yeetus'
     };
+
+  }
+
+  competencyStatusIcon(): string {
+    switch(this.competency.status) {
+      case Lifecycles.DRAFT:
+        return 'far fa-file-edit fa-2x';
+      case Lifecycles.SUBMITTED:
+        return 'far fa-file-exclamation fa-2x';
+      case Lifecycles.PUBLISHED:
+        return 'far fa-file-checked fa-2x';
+      case Lifecycles.DEPRECATED:
+        return 'far fa-file-minus fa-2x';
+      case Lifecycles.REJECTED:
+        return 'far fa-file-times fa-2x';
+    }
+  }
+
+  adminButtonText(): string {
+    switch (this.competency.status) {
+      case Lifecycles.SUBMITTED:
+        return 'PUBLISH';
+      case Lifecycles.PUBLISHED:
+        return 'DEPRECATE';
+      case Lifecycles.DEPRECATED:
+        return 'EDIT';
+      default:
+        return 'yeet';
+    }
+  }
+
+  onAdminButtonClicked(): void {
+    switch (this.competency.status) {
+      case Lifecycles.SUBMITTED:
+        this.onPublish();
+        break;
+      case Lifecycles.PUBLISHED:
+        this.onDeprecate();
+        break;
+      case Lifecycles.DEPRECATED:
+        this.onUpdateSubmission();
+        break;
+      default:
+        break;
+    }
   }
 
   /**
@@ -35,4 +82,18 @@ export class PreviewCompetencyComponent implements OnInit {
     this.updateSubmission.emit();
   }
 
+  onPublish(): void {
+    // TODO: Define this one
+    this.competency.status = Lifecycles.PUBLISHED;
+  }
+
+  onDeprecate(): void {
+    // TODO: fill this one
+    this.competency.status = Lifecycles.DEPRECATED;
+  }
+
+  onReject(): void {
+    // TODO: yada yada
+    this.competency.status = Lifecycles.REJECTED;
+  }
 }
