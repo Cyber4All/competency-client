e0a2b82af6dcbc563886fb1a674211845c01689b
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2023-04-20 14:38:22 -0400

    created help pages components

--- src/app/shared/pages/pages.module.ts
+++ src/app/shared/pages/pages.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HelpPageComponent } from './help-page/help-page.component';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
+import { DowntimeComponent } from './downtime/downtime.component';
+import { BetaWelcomeComponent } from './beta-welcome/beta-welcome.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HelpPageComponent,
+    TermsOfServiceComponent,
+    DowntimeComponent,
+    BetaWelcomeComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HelpPageComponent,
+    TermsOfServiceComponent,
+    DowntimeComponent,
+    BetaWelcomeComponent
   ]
 })
 export class PagesModule { }
