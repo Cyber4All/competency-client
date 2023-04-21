2b4f35b9030fa8696aeaf00de8da9cf6d98eac4b
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2023-04-21 09:58:48 -0400

    created custom color palette for angular material; added some sections of help pages, need to refine content; removed preset angular material styles

---
+++ src/app/shared/pages/help-page/copy.ts
@@ -0,0 +1,139 @@
+export const sections = {
+    elements: {
+        title: 'Competency Constructor',
+        topics: [
+            {
+                title: 'Building a Competency',
+                questions: [
+                    {
+                        question: 'Getting Started',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                    {
+                        question: 'Actor',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                    {
+                        question: 'Behavior',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                    {
+                        question: 'Context & Conditions',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                    {
+                        question: 'Degree',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                    {
+                        question: 'Employability',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                    {
+                        question: 'Notes',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                    {
+                        question: 'Submitting a Competency',
+                        answer: '<p> Anyone can search for a learning object on CLARK through the search bar or filter ' +
+                            'function. However, you must register for an account and be logged in to preview and download any learning ' +
+                            'object material. We have a great tutorial that can guide you step by step on searching and downloading ' +
+                            'here, <a href= "https://youtu.be/ymerRpfbnQQ">https://youtu.be/ymerRpfbnQQ</a>.'
+                    },
+                ]
+            },
+            {
+                title: 'Author Dashboard',
+                questions: [
+                    {
+                        question: 'What is a learning object?',
+                        answer: 'A <em>learning object</em> is a free piece of curriculum that is used to teach cyber and cyber-related ' +
+                            'material in varying types of classrooms. Every learning object is created by a registered CLARK author. ' +
+                            'Learning objects represent the main content found on CLARK.'
+                    },
+                    {
+                        question: 'How does a learning object work?',
+                        answer: 'A learning object is made up of different components. Each learning object will have a description ' +
+                            'of the curriculum along with learning outcomes, academic levels, and materials related to the learning ' +
+                            'object. All registered CLARK users are able to download the learning object files and ancillary materials ' +
+                            '(some learning objects might be URL link-based only). All registered authors can also save learning ' +
+                            'objects to their personalized CLARK library.'
+                    }
+                ]
+            },
+            {
+                title: `A Competnency's Lifecycle`,
+                questions: [
+                    {
+                        question: 'A Draft Competency',
+                        answer: 'A <em>learning object</em> is a free piece of curriculum that is used to teach cyber and cyber-related ' +
+                            'material in varying types of classrooms. Every learning object is created by a registered CLARK author. ' +
+                            'Learning objects represent the main content found on CLARK.'
+                    },
+                    {
+                        question: 'A Submitted Competency',
+                        answer: 'A <em>learning object</em> is a free piece of curriculum that is used to teach cyber and cyber-related ' +
+                            'material in varying types of classrooms. Every learning object is created by a registered CLARK author. ' +
+                            'Learning objects represent the main content found on CLARK.'
+                    },
+                    {
+                        question: 'A Published Competency',
+                        answer: 'A <em>learning object</em> is a free piece of curriculum that is used to teach cyber and cyber-related ' +
+                            'material in varying types of classrooms. Every learning object is created by a registered CLARK author. ' +
+                            'Learning objects represent the main content found on CLARK.'
+                    },
+                    {
+                        question: 'A Deprecated Competency',
+                        answer: 'A <em>learning object</em> is a free piece of curriculum that is used to teach cyber and cyber-related ' +
+                            'material in varying types of classrooms. Every learning object is created by a registered CLARK author. ' +
+                            'Learning objects represent the main content found on CLARK.'
+                    },
+                    {
+                        question: 'A Rejected Competency',
+                        answer: 'A <em>learning object</em> is a free piece of curriculum that is used to teach cyber and cyber-related ' +
+                            'material in varying types of classrooms. Every learning object is created by a registered CLARK author. ' +
+                            'Learning objects represent the main content found on CLARK.'
+                    },
+                ]
+            },
+        ]
+    },
+    information: {
+        title: 'About',
+        topics: [
+            {
+                title: 'About the System',
+                questions: [
+                    {
+                        question: 'What is Competency Constructor?',
+                        answer: 'CLARK, the Cybersecurity Labs and Resource Knowledge-base, is a platform for building and sharing free ' +
+                            'cybersecurity curricula. It includes a model for building curriculum, the digital library system, and ' +
+                            'distinct curriculum collections.'
+                    },
+                ]
+            }
+
+        ]
+    },
+};
