f0bc57f759232f7418f03cacd0899974bd40b1c5
Paulo Manalo <pmanal1@students.towson.edu>
2023-04-27 14:01:42 -0400

    Adds filtering dropdowns

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DropdownType } from 'entity/dropdown';
 
 @Component({
   selector: 'cc-admin-dashboard',
@@ -9,7 +10,43 @@ export class AdminDashboardComponent implements OnInit {
 
   constructor() { }
 
+  dropdownType = DropdownType;
+
+  // Selected Filters
+  selectedStatuses: string[] = [];
+  selectedWorkroles: string[] = [];
+  selectedTasks: string[] = [];
+  selectedAudiences: string[] = [];
+
   ngOnInit(): void {
   }
 
+  filter() {
+    // TODO: Implement filtering
+    // Emit the selected filters
+    // this.filterCompetencies.emit({
+    //   status: this.selectedStatuses,
+    //   workrole: this.selectedWorkroles,
+    //   task: this.selectedTasks,
+    //   audience: this.selectedAudiences
+    // });
+  }
+
+  // Functions to set the selected filters upon change
+  statuses(statuses: string[]) {
+    this.selectedStatuses = statuses;
+    this.filter();
+  }
+  workroles(workroles: string[]) {
+    this.selectedWorkroles = workroles;
+    this.filter();
+  }
+  tasks(tasks: string[]) {
+    this.selectedTasks = tasks;
+    this.filter();
+  }
+  audiences(audiences: string[]) {
+    this.selectedAudiences = audiences;
+    this.filter();
+  }
 }
