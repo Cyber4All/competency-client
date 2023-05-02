276f7506370ea2e6fe5b8f33c178a912b6e8824f
Paulo Manalo <pmanal1@students.towson.edu>
2023-05-02 13:12:10 -0400

    Moves adminHeader to directive

--- src/app/shared/components/competency-card/competency-card.component.ts
+++ src/app/shared/components/competency-card/competency-card.component.ts
@@ -21,7 +21,6 @@ export class CompetencyCardComponent implements OnInit {
   ) {}
 
   async ngOnInit(): Promise<void> {
-    console.log(this.competency);
     this.loading = true;
     // load workrole
     if (this.competency.behavior.work_role) {
--- src/app/shared/directives/panel/panel-viewer/panel-viewer.component.ts
+++ src/app/shared/directives/panel/panel-viewer/panel-viewer.component.ts
@@ -10,6 +10,7 @@ import { BehaviorSubject, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { PanelOptions } from '../panel.directive';
 import { fade } from '../panel.animations';
+import { Lifecycles } from 'entity/Lifecycles';
 @Component({
   selector: 'cc-panel-viewer',
   template: `
@@ -26,7 +27,7 @@ import { fade } from '../panel.animations';
           'side-panel--no-padding': options && !options.padding
         }"
       >
-        <div class="side-panel__header">
+        <div class="side-panel__header" *ngIf="!options.isAdmin else adminHeader">
           <h3 class="side-panel__title">{{ options.title }}</h3>
           <div class="side-panel__buttons">
             <div
@@ -49,6 +50,24 @@ import { fade } from '../panel.animations';
         <ng-content></ng-content>
       </div>
     </ng-container>
+
+    <ng-template #adminHeader>
+      <div class="admin header" [ngClass]="options.competency.status.toString()">
+        <div class="left-corner">
+          <h2>{{ options.competency.behavior.work_role }} - {{ options.competency.behavior.tasks[0].split(":")[0] }}</h2>
+          <p><b>Task: </b>{{ options.competency.behavior.tasks[0] }}</p>
+        </div>
+        <div class="right-corner">
+          <div class="status">
+            <i [ngClass]="competencyStatusIcon()"></i>
+            <p><b>{{ options.competency.status | titlecase }}</b></p>
+          </div>
+          <button (click)="close.emit()">
+            <i class="far fa-times fa-2x"></i>
+          </button>
+        </div>
+      </div>
+    </ng-template>
   `,
   styleUrls: ['./panel-viewer.component.scss'],
   animations: [fade],
@@ -116,5 +135,26 @@ export class PanelViewerComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroyed$.next();
     this.destroyed$.unsubscribe();
+  }
+
+  /**
+   * Returns an icon to display depending on competency status
+   * Only for admin view
+   *
+   * @returns A FontAwesome icon
+   */
+  competencyStatusIcon(): string {
+    switch(this.options.competency.status) {
+      case Lifecycles.DRAFT:
+        return 'far fa-file-edit fa-2x';
+      case Lifecycles.SUBMITTED:
+        return 'far fa-file-exclamation fa-2x';
+      case Lifecycles.PUBLISHED:
+        return 'far fa-file-check fa-2x';
+      case Lifecycles.DEPRECATED:
+        return 'far fa-file-minus fa-2x';
+      case Lifecycles.REJECTED:
+        return 'far fa-file-times fa-2x';
+    }
   }
 }
